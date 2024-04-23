c89bbf43ae02a956ddbcd862677295b81ea251db
Johnnyyu3 <jyy3@njit.edu>
2024-04-23 17:21:17 -0400

    reworked pedometer to a fragment class

--- app/src/main/java/com/group16/fitapp/Pedometer.kt
+++ app/src/main/java/com/group16/fitapp/Pedometer.kt
@@ -7,54 +7,60 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 
-class Pedometer : AppCompatActivity(),SensorEventListener {
+class Pedometer : Fragment(R.layout.frag_pedometer),SensorEventListener {
     private var sensorManager: SensorManager? = null
     private var running = false
     private var totalSteps = 0f
     private var previousTotalSteps = 0f
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View{
+        val view = inflater.inflate(R.layout.frag_pedometer, container, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.step_count)
-
         loadData()
         resetSteps()
+        return view
     }
     override fun onResume(){
         super.onResume()
         running = true
         val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
         if(stepSensor == null){
-            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
         } else {
             sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        var tv_stepsTaken = view?.findViewById<TextView>(R.id.tv_stepsTaken)
         if( running) {
             totalSteps = event!!.values[0]
 
             val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
 
-            tv_stepsTaken.text = ("$currentSteps")
+            tv_stepsTaken?.text = ("$currentSteps")
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         TODO("Not yet implemented")
     }
-    fun resetSteps() {
-        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-        tv_stepsTaken.setOnClickListener{
-            Toast.makeText(this, "Long Tap to reset steps", Toast.LENGTH_SHORT).show()
+    private fun resetSteps() {
+        var tv_stepsTaken = view?.findViewById<TextView>(R.id.tv_stepsTaken)
+        tv_stepsTaken?.setOnClickListener{
+            Toast.makeText(activity, "Long Tap to reset steps", Toast.LENGTH_SHORT).show()
         }
-        tv_stepsTaken.setOnLongClickListener{
+        tv_stepsTaken?.setOnLongClickListener{
             previousTotalSteps = totalSteps
             tv_stepsTaken.text = 0.toString()
             saveData()
@@ -62,18 +68,20 @@ class Pedometer : AppCompatActivity(),SensorEventListener {
         }
     }
     private fun saveData() {
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val sharedPreferences = this.activity?.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
 
-        val editor = sharedPreferences.edit()
-        editor.putFloat("key1", previousTotalSteps)
-        editor.apply()
+        val editor = sharedPreferences?.edit()
+        editor?.putFloat("key1", previousTotalSteps)
+        editor?.apply()
     }
     private fun loadData() {
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        val sharedPreferences = this.activity?.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences?.getFloat("key1", 0f)
 
         Log.d("Pedometer", "$savedNumber")
 
-        previousTotalSteps = savedNumber
+        if (savedNumber != null) {
+            previousTotalSteps = savedNumber
+        }
     }
 }
